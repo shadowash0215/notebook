d8dd11899a3c9b40d21b6a70d9f091501477af31
shad0wash <shadowash215@gmail.com>
2025-02-18 17:47:43 +0800

    Deployed add7efc with MkDocs version: 1.4.2

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -3,7 +3,22 @@ window.MathJax = {
       inlineMath: [["\\(", "\\)"]],
       displayMath: [["\\[", "\\]"]],
       processEscapes: true,
-      processEnvironments: true
+      processEnvironments: true,
+      macros: {
+        op: ['\\operatorname{#1}', 1],
+        from: '\\leftarrow',
+        abs: ['\\left\\lvert#1\\right\\rvert', 1],
+        norm: ['\\left\\lVert#1\\right\\rVert', 1],
+        Pr: ['\\operatorname{Pr}\[#1\]', 1],
+        innerproduct: ['\\left\\langle#1 \\vert #2\\right\\rangle', 2],
+        ket: ['\\left\\lvert#1\\right\\rangle', 1],
+        bra: ['\\left\\langle#1\\right\\rvert', 1],
+        poly: ['\\operatorname{poly}\(#1\)', 1],
+        PRF: '\\mathsf{PRF}',
+        Adv: '\\operatorname{Adv}',
+        sk: '\\mathsf{sk}',
+        pk: '\\mathsf{pk}',
+      }
     },
     options: {
       ignoreHtmlClass: ".*|",
@@ -12,5 +27,8 @@ window.MathJax = {
   };
   
   document$.subscribe(() => { 
+    MathJax.startup.output.clearCache()
+    MathJax.typesetClear()
+    MathJax.texReset()
     MathJax.typesetPromise()
   })
